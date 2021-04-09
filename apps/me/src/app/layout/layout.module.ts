fdae25005e795b8cabed795a6954bb560eb742c8
Josh Tune <joshua.tune@gmail.com>
2021-04-08 22:36:51 -0600

    layout setup

--- apps/me/src/app/layout/layout.module.ts
+++ apps/me/src/app/layout/layout.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import {RouterModule} from "@angular/router";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, SharedModule],
 })
 export class LayoutModule {}
